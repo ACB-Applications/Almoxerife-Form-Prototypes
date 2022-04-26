9896d2c1a47833bd1a3ddb973da749aeb5a20c9d
Marlinsk <Marlonnlmx@outlook.com>
2022-04-26 15:59:44 -0300

    Adding special character lockout to input

---
+++ forms/Formulario RM Almox3/lockout.js
@@ -0,0 +1,8 @@
+$("[name=usernameDataSUL]").on("keypress", function (event) {
+  var regex = new RegExp("^[a-zA-Z0-9_ ]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
