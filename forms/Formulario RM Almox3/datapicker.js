0af0fdbbdf36891c09da7639f671cb3c48a65c5a
Marlinsk <Marlonnlmx@outlook.com>
2022-04-29 10:45:06 -0300

    Adding one more calendar field

--- forms/Formulario RM Almox3/datapicker.js
+++ forms/Formulario RM Almox3/datapicker.js
@@ -2,3 +2,8 @@ FLUIGC.calendar("#dataRequisicao", {
   pickDate: true,
   pickTime: false,
 });
+
+FLUIGC.calendar("#dataEntrega", {
+  pickDate: true,
+  pickTime: false,
+});
--- forms/Formulario RM Almox3/maskInput.js
+++ forms/Formulario RM Almox3/maskInput.js
@@ -1,1 +1,2 @@
 $("[id=dataRequisicao]").mask("00/00/0000");
+$("[id=dataEntrega]").mask("00/00/0000");
