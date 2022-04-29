bdd2ef8e249f7951e65816d0c2ab271376def29e
Marlinsk <Marlonnlmx@outlook.com>
2022-04-29 10:14:47 -0300

    Update

--- forms/Formulario RM Almox3/lockout.js
+++ forms/Formulario RM Almox3/lockout.js
@@ -1,5 +1,14 @@
 $("[name=usernameDataSUL]").on("keypress", function (event) {
-  var regex = new RegExp("^[a-zA-Z0-9_ ]+$");
+  var regex = new RegExp("^[a-zA-Z0-9_]+$");
+  var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+  if (!regex.test(key)) {
+    event.preventDefault();
+    return false;
+  }
+});
+
+$("[name=codigoItem]").on("keypress", function (event) {
+  var regex = new RegExp("^[0-9]+$");
   var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
   if (!regex.test(key)) {
     event.preventDefault();
